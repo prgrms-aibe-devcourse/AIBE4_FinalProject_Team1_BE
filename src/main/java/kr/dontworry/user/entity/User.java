e2321ab0993d029a8a15c229a2819cdbe7b57d6a
Glory0206 <dmb07223@gmail.com>
2026-01-29 20:43:39 +0900

    feat: user 관련 entity 작성

---
+++ src/main/java/kr/dontworry/user/entity/User.java
@@ -0,0 +1,50 @@
+package kr.dontworry.user.entity;
+
+import jakarta.persistence.*;
+import kr.dontworry.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID publicId;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<SocialAccount> socialAccounts = new ArrayList<>();
+
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    public void generatePublicId() {
+        this.publicId = UUID.randomUUID();
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public void withdraw() {
+        this.deletedAt = LocalDateTime.now();
+    }
+}
