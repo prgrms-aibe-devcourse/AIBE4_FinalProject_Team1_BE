3b4d1867a425d7854a9ef2fc8620f4695f7b770c
Glory0206 <dmb07223@gmail.com>
2026-01-30 14:53:23 +0900

    feat: session 미사용

--- src/main/java/kr/dontworry/global/config/SecurityConfig.java
+++ src/main/java/kr/dontworry/global/config/SecurityConfig.java
@@ -1,14 +1,15 @@
 package kr.dontworry.global.config;
 
-import kr.dontworry.global.auth.filter.JwtAuthenticationFilter;
-import kr.dontworry.global.auth.jwt.JwtProvider;
 import kr.dontworry.auth.service.CustomOAuth2UserService;
+import kr.dontworry.global.auth.filter.JwtAuthenticationFilter;
 import kr.dontworry.global.auth.handler.OAuth2SuccessHandler;
+import kr.dontworry.global.auth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,6 +25,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login/**", "/error").permitAll()
                         .anyRequest().authenticated()
