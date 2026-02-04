8f21c37b4e8b0a7cbc56153aa9a2dd0042020536
Glory0206 <dmb07223@gmail.com>
2026-02-04 19:11:58 +0900

    feat: category exception

---
+++ src/main/java/kr/dontworry/domain/category/exception/CategoryErrorCode.java
@@ -0,0 +1,16 @@
+package kr.dontworry.domain.category.exception;
+
+import kr.dontworry.global.exception.ErrorModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CategoryErrorCode implements ErrorModel {
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "C001", "카테고리를 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
