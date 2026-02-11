6e8f7eb9012d32e246ef59769e31534ae696915d
박지훈 <jihun6548@naver.com>
2026-02-11 18:00:45 +0900

    fix: gradle 설정 변경 SonarCloud

--- src/main/java/kr/inventory/Application.java
+++ src/main/java/kr/inventory/Application.java
@@ -3,7 +3,6 @@ package kr.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TEST
 @SpringBootApplication
 public class Application {
 
