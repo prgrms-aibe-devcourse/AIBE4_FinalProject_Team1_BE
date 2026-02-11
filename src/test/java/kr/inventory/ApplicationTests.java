779adfc06d4c99fa9b12aba2b106b019e0e33875
박지훈 <jihun6548@naver.com>
2026-02-11 17:23:18 +0900

    feat: jacoco, SonarCloud 설정 추가

--- src/test/java/kr/inventory/ApplicationTests.java
+++ src/test/java/kr/inventory/ApplicationTests.java
@@ -1,13 +1,13 @@
-package kr.inventory;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// package kr.inventory;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class ApplicationTests {
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
