a4c7b043a9e8abb1e6addce59074b776e9d62e2d
박지훈 <jihun6548@naver.com>
2026-02-11 17:47:22 +0900

    test: cloud 테스트

--- src/main/java/kr/inventory/Application.java
+++ src/main/java/kr/inventory/Application.java
@@ -3,6 +3,7 @@ package kr.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TEST
 @SpringBootApplication
 public class Application {
 
