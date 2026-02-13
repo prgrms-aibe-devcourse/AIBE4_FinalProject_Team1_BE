a595c3146752f5a6ee32afd03582bc505ebcf793
ehdghksahd <ehdghksahd@naver.com>
2026-02-13 10:11:18 +0900

    refactor: dto 명 response로 변경

--- src/main/java/kr/inventory/domain/document/controller/dto/ocr/OcrResultResponse.java
+++ src/main/java/kr/inventory/domain/document/controller/dto/ocr/OcrResultResponse.java
@@ -3,6 +3,6 @@ package kr.inventory.domain.document.controller.dto.ocr;
 import java.util.List;
 
 public record OcrResultResponse(
-	List<ReceiptData> results
+	List<ReceiptResponse> results
 ) {
 }
