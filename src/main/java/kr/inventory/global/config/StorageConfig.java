be7063e192906182a814c7f51f3b913bcfd345e2
ehdghksahd <ehdghksahd@naver.com>
2026-02-19 18:01:08 +0900

    fix: merge후 겹치는부분 수정

--- src/main/java/kr/inventory/global/config/StorageConfig.java
+++ src/main/java/kr/inventory/global/config/StorageConfig.java
@@ -14,28 +14,28 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class StorageConfig {
-	@Value("${cloud.aws.credentials.access-key}")
-	String accessKey;
-	@Value("${cloud.aws.credentials.secret-key}")
-	String secretKey;
-	@Value("${cloud.aws.region.static}")
-	String region;
-
-	@Bean
-	public S3Client s3Client() {
-		return S3Client.builder()
-			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(
-				AwsBasicCredentials.create(accessKey, secretKey)))
-			.build();
-	}
-
-	@Bean
-	public S3Presigner s3Presigner() {
-		return S3Presigner.builder()
-			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(
-				AwsBasicCredentials.create(accessKey, secretKey)))
-			.build();
-	}
+	// @Value("${cloud.aws.credentials.access-key}")
+	// String accessKey;
+	// @Value("${cloud.aws.credentials.secret-key}")
+	// String secretKey;
+	// @Value("${cloud.aws.region.static}")
+	// String region;
+	//
+	// @Bean
+	// public S3Client s3Client() {
+	// 	return S3Client.builder()
+	// 		.region(Region.of(region))
+	// 		.credentialsProvider(StaticCredentialsProvider.create(
+	// 			AwsBasicCredentials.create(accessKey, secretKey)))
+	// 		.build();
+	// }
+	//
+	// @Bean
+	// public S3Presigner s3Presigner() {
+	// 	return S3Presigner.builder()
+	// 		.region(Region.of(region))
+	// 		.credentialsProvider(StaticCredentialsProvider.create(
+	// 			AwsBasicCredentials.create(accessKey, secretKey)))
+	// 		.build();
+	// }
 }
