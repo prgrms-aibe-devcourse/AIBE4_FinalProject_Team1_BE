315305d68d3ff0e142f172cb3e0a2c8e40d61710
ehdghksahd <ehdghksahd@naver.com>
2026-02-26 10:15:13 +0900

    feat: 입고에 관한 기능 구현중

--- src/main/java/kr/inventory/domain/stock/repository/StockInboundItemRepositoryCustom.java
+++ src/main/java/kr/inventory/domain/stock/repository/StockInboundItemRepositoryCustom.java
@@ -1,12 +1,4 @@
 package kr.inventory.domain.stock.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.inventory.domain.stock.controller.dto.StockInboundItemResponse;
-import kr.inventory.domain.stock.entity.StockInbound;
-import kr.inventory.domain.stock.entity.StockInboundItem;
-
 public interface StockInboundItemRepositoryCustom {
 }
