0ff298586cf04ca7bb779f21d31136abcd0ec2f3
Glory0206 <dmb07223@gmail.com>
2026-02-27 11:32:39 +0900

    rename: table session controller 명칭 수정

--- src/main/java/kr/inventory/domain/dining/controller/TableSessionController.java
+++ src/main/java/kr/inventory/domain/dining/controller/TableSessionController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/table-sessions")
-public class TableSessionServiceController {
+public class TableSessionController {
     private final TableSessionService tableSessionService;
 
     @PostMapping("/enter")
